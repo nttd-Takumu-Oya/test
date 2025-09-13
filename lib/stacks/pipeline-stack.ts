88bd1559c50a495ddd829c3130493f1ff6ded1e0
tkm <Takumu.Oya@jp.nttdata.com>
2025-09-13 09:21:56 +0000

    Initial commit

---
+++ lib/stacks/pipeline-stack.ts
@@ -0,0 +1,27 @@
+// lib/stacks/pipeline-stack.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Secrets ManagerからGitHubトークンを取得
+    const githubToken = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubToken', 'github');
+    // GitHubリポジトリ情報
+    const repo = 'nttd-Takumu-Oya/test';
+    const branch = 'master';
+
+    const pipeline = new CodePipeline(this, 'Pipeline',{
+      pipelineName: 'test-pipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(repo, branch, {
+          authentication: githubToken.secretValueFromJson('github'),
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
+  }
+}
